730f280349861818fbe532f51461a914c17e96f5
MalikeChapman <ashchapman330@gmail.com>
2021-02-15 14:57:47 -0500

    Finished required stories for unit two.

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -5,15 +5,25 @@ import android.util.Log;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
+
+    int movieID;
     String backdropPath;
     String posterPath;
     String title;
     String overview;
+    double rating;
+
+    // Empty constructor needed by Parcler Library
+    public Movie(){
+
+    }
 
 
     public Movie(JSONObject jsonObject)throws JSONException{
@@ -22,6 +32,8 @@ public class Movie {
            posterPath = jsonObject.getString("poster_path");
             title = jsonObject.getString("title");
             overview = jsonObject.getString("overview");
+            rating = jsonObject.getDouble("vote_average");
+            movieID = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsdonArray(JSONArray movieJsonArray)throws JSONException{
@@ -31,6 +43,10 @@ public class Movie {
         }
         return movies;
 
+    }
+
+    public int getMovieID() {
+        return movieID;
     }
 
     public String getPosterPath() {
@@ -47,5 +63,9 @@ public class Movie {
 
     public String getBackdropPath() {
         return String.format("https://image.tmdb.org/t/p/w342/%s", backdropPath);
+    }
+
+    public double getRating() {
+        return rating;
     }
 }
